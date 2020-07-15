f9bcc6b4a123ade04a27f091dfa4d7acb7aa8be3
Taylor Fraley <tfraley@clearent.com>
2020-07-15 17:39:21 -0400

    move tests folder up one level; implement basic KvStore type

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,19 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+pub struct KvStore;
+
+impl KvStore {
+  pub fn new() -> KvStore {
+    unimplemented!();
+  }
+
+  pub fn set(self: &KvStore, key: String, value: String) {
+    unimplemented!();
+  }
+
+  pub fn get(self: &KvStore, key: String) -> Option<String> {
+    unimplemented!();
+  }
+
+  pub fn remove(self: &KvStore, key: String) {
+    unimplemented!();
+  }
 }
